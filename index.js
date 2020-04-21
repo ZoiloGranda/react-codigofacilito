b802316d5d4d187624547f6671174f31a65551ae
ZoiloGranda <zgranda@gmail.com>
2020-04-21 18:06:12 -0400

    Separar Componentes

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
-import React, { Component, useState } from "react";
+import React, { Component } from "react";
 import { render } from "react-dom";
 import Hello from "./Hello";
 import "./style.css";
-
-function Contador(props) {
-  const [contador, setContador]= useState(0);
-  return (
-    <div>
-      <p>Conteo: {contador}</p>
-      <button onClick={()=>setContador(contador+1)}>Aumentar</button>
-    </div>
-  );
-}
+import Contador from './Contador';
 
 class App extends Component {
   constructor() {
